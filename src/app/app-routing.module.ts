cb0b900f9ce724c4ecdac56f5cf610e45de55db5
oscarfont <o.font@alumnos.upm.es>
2021-02-10 13:45:49 +0100

    routes change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {anchorScrolling: 'enabled'})],
+  imports: [RouterModule.forRoot(routes, {anchorScrolling: 'enabled', useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
