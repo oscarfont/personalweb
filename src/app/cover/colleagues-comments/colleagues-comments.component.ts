9c301cd00dbe0e72939f0d39d7ec5dc15eb21a38
ofont <o.font@alumnos.upm.es>
2022-11-19 21:24:28 +0100

    colleagues comments added

--- src/app/cover/colleagues-comments/colleagues-comments.component.ts
+++ src/app/cover/colleagues-comments/colleagues-comments.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AppStateService } from 'src/app/app-state.service';
 
 @Component({
   selector: 'app-colleagues-comments',
@@ -8,13 +9,14 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ColleaguesCommentsComponent implements OnInit {
 
   @Input() background: any;
+  isMobile: boolean;
 
-  constructor() {
-    console.log(this.background)
+  constructor(private appStateService: AppStateService) {
+    //console.log(this.background)
   }
 
   ngOnInit(): void {
-
+    this.isMobile = this.appStateService.getIsMobileResolution();
   }
 
 }
