e620889ba4094608f0a4e6caeddea4887f62cb71
ofont <o.font@alumnos.upm.es>
2023-03-01 23:18:48 +0100

    changed return type of image uploaded

--- src/app/blog/new-post/new-post.component.ts
+++ src/app/blog/new-post/new-post.component.ts
@@ -52,7 +52,7 @@ export class NewPostComponent {
           const imageUrl = `${environment.backendHost}/public/${resObj.data}`;
           this.image.insert(imageUrl, false, null, this.image.get(), { link: imageUrl });
           blogService.addPostMedia(resObj.data);
-          return false;
+          return true;
         },
         'image.removed': function (img: any) {
           const fileName = img[0].src.split('/')[3];
