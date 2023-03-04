2f3a117d6396a72c1d18b0c4f50d4333e973842a
ofont <o.font@alumnos.upm.es>
2023-03-04 19:48:37 +0100

    added enctype to request

--- src/services/utils.service.ts
+++ src/services/utils.service.ts
@@ -31,6 +31,7 @@ export class UtilsService {
     const headers = new HttpHeaders({
       'accept': 'application/json',
       'Accept-Language': 'en-US,en;q=0.8',
+      'enctype': 'multipart/form-data',
       'Content-Type': `multipart/form-data`
     })
     return this.http.post<any>(this.baseURL + '/utils/uploadImage/', formData, { headers: headers });
